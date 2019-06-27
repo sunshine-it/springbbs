798553f22590d86fb355b1989e64c1f96b20448a
vagrant <vagrant@homestead>
2019-06-27 12:45:20 +0000

    生成话题骨架

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,5 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 // Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 // // 处理 edit 页面提交的更改
 // Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
