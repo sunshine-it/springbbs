6cf30b1baa72ca406fbf0136d5184d90622241c6
vagrant <vagrant@homestead>
2019-07-01 10:18:51 +0000

    修复 XSS 注入漏洞

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -12,6 +12,8 @@ class TopicObserver
     // 监听数据即将保存的事件
     public function saving(Topic $topic)
     {
+        // 在数据入库前进行 XSS 过滤 (配置 HTMLPurifier for Laravel 5)
+        $topic->body = clean($topic->body, 'user_topic_body');
         $topic->excerpt = make_excerpt($topic->body);
     }
 
