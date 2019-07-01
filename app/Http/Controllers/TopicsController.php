deedf8f62653bde87e75a155d73acbfc57a16de0
vagrant <vagrant@homestead>
2019-07-01 10:39:59 +0000

    用户可以编辑话题

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -54,16 +54,19 @@ class TopicsController extends Controller
 
 	public function edit(Topic $topic)
 	{
+        // 授权策略的调用
         $this->authorize('update', $topic);
-		return view('topics.create_and_edit', compact('topic'));
+        $categories = Category::all();
+		return view('topics.create_and_edit', compact('topic','categories'));
 	}
 
 	public function update(TopicRequest $request, Topic $topic)
 	{
+        // 授权策略的调用
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('success', '更新成功！');
 	}
 
 	public function destroy(Topic $topic)
@@ -71,7 +74,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('success', '删除成功！');
 	}
 
     // 图片上传
--- app/Policies/TopicPolicy.php
+++ app/Policies/TopicPolicy.php
@@ -9,8 +9,9 @@ class TopicPolicy extends Policy
 {
     public function update(User $user, Topic $topic)
     {
-        // return $topic->user_id == $user->id;
-        return true;
+        // update() 方法进行修改，只有当话题关联作者的 ID 等于当前登录用户 ID 时候才能修改
+        return $topic->user_id == $user->id;
+        // return true;
     }
 
     public function destroy(User $user, Topic $topic)
