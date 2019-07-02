2a36c95cc264e425bff52f979a76018337873758
vagrant <vagrant@homestead>
2019-07-02 12:00:36 +0000

    翻译队列化

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,23 +3,32 @@
 namespace App\Observers;
 
 use App\Models\Topic;
-use App\Handlers\SlugTranslateHandler; // 使用 Guzzle 及 拼音
+// 队列执行
+use App\Jobs\TranslateSlug;
 
 // creating, created, updating, updated, saving,
 // saved,  deleting, deleted, restoring, restored
 // 帖子模型观察器
 class TopicObserver
 {
-    // 监听数据即将保存的事件
+    // 监听数据即将保存的事件 此事件发生在创建和编辑时、数据入库之前
     public function saving(Topic $topic)
     {
-        // 在数据入库前进行 XSS 过滤 (配置 HTMLPurifier for Laravel 5)
+        // XSS 过滤 (配置 HTMLPurifier for Laravel 5)
         $topic->body = clean($topic->body, 'user_topic_body');
+
         // 生成话题摘录
         $topic->excerpt = make_excerpt($topic->body);
+    }
+
+    // 监听处理完成后的操作 此事件发生在创建和编辑时、数据入库以后
+    public function saved(Topic $topic)
+    {
         // 如 slug 字段无内容，即使用翻译器对 title 进行翻译
         if ( ! $topic->slug) {
-            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+
+            // 推送任务到队列
+            dispatch(new TranslateSlug($topic));
         }
     }
 
