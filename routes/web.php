05bb4002bb8968797174e17dfa2bcfa3e1e948e2
vagrant <vagrant@homestead>
2019-07-06 04:09:01 +0000

    管理后台权限

--- routes/web.php
+++ routes/web.php
@@ -61,3 +61,5 @@ Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload
 Route::resource('replies', 'RepliesController', ['only' => ['store','destroy']]);
 // 通知列表
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
+// 无权限提醒页面
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
